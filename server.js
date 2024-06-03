512f4cad57eab13821bede1482763e40c259085b
kevokario <kevokario@gmail.com>
2024-06-03 12:43:30 +0300

    update readme file

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000; // Use the provided environment port or default to 3000
+const PORT= 3000; // Use the provided environment port or default to 3000
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
